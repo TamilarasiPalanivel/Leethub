a2bad0979a761e71933667b810942997f7eb31b6
TamilarasiPalanivel <168735036+TamilarasiPalanivel@users.noreply.github.com>
2024-10-09 23:25:44 +0530

    Time: 0 ms (100%), Space: 41.6 MB (28.39%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,22 +1,19 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        // Variables to track unbalanced parentheses
         int open = 0, close = 0;
         
-        // Iterate through the string
         for (char c : s.toCharArray()) {
             if (c == '(') {
-                open++; // Found an opening parenthesis
+                open++; 
             } else {
                 if (open > 0) {
-                    open--; // Found a closing parenthesis that matches an opening one
+                    open--; 
                 } else {
-                    close++; // Found a closing parenthesis without a match
+                    close++; 
                 }
             }
         }
         
-        // Total unbalanced parentheses is the sum of unmatched opening and closing
         return open + close;
     }
 }
