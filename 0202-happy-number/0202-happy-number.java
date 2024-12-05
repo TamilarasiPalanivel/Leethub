31fc303b64686c17e9ea709a5e4df55ed83bcf96
TAMILARASI P <tamilarasip.22cse@kongu.edu>
2024-12-06 00:05:19 +0530

    Time: 1 ms (74.25%), Space: 40.4 MB (88.55%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getSumOfSquares(n);
+        }
+        
+        return n == 1;
+    }
+
+    private int getSumOfSquares(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+}
