4e2cf95fee6ebf77b931b88fe880de41bc7e0770
TAMILARASI P <tamilarasip.22cse@kongu.edu>
2025-01-28 16:28:45 +0530

    Time: 4 ms (28.7%), Space: 45.4 MB (91.63%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,36 +1,30 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int totalCandies = n;
-        int i = 1;
-
-        while (i < n) {
-            if (ratings[i] == ratings[i - 1]) {
-                i++;
-                continue;
+        int candies=0,i;
+        int[] left=new int[n];
+        int[] right=new int[n];
+        Arrays.fill(left,1);
+        Arrays.fill(right,1);
+        for(i=1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                left[i]=left[i-1]+1;
             }
-
-            int currentPeak = 0;
-            while (i < n && ratings[i] > ratings[i - 1]) {
-                currentPeak++;
-                totalCandies += currentPeak;
-                i++;
-            }
-
-            if (i == n) {
-                return totalCandies;
-            }
-
-            int currentValley = 0;
-            while (i < n && ratings[i] < ratings[i - 1]) {
-                currentValley++;
-                totalCandies += currentValley;
-                i++;
-            }
-
-            totalCandies -= Math.min(currentPeak, currentValley);
         }
-
-        return totalCandies;        
+        for(i=n-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                right[i]=right[i+1]+1;
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            candies=candies+Math.max(left[i],right[i]);
+        }
+return candies;
+                
     }
 }
