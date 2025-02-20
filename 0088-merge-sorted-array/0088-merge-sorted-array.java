b6e20b512cc926e3b331ed800016e7885dd28708
TAMILARASI P <tamilarasip.22cse@kongu.edu>
2025-02-20 23:11:08 +0530

    Time: 0 ms (100%), Space: 42.3 MB (41.61%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,20 +1,17 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // Initialize pointers for nums1 and nums2 and the merged array index
-        int i = m - 1;  // Pointer for the last element in nums1 (ignoring the zeros)
-        int j = n - 1;  // Pointer for the last element in nums2
-        int k = m + n - 1;  // Pointer for the last position in nums1
+        int i = m - 1;
+        int j = n - 1; 
+        int k = m + n - 1; 
 
-        // Start merging from the back of nums1 and nums2
         while (i >= 0 && j >= 0) {
             if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];  // Place nums1[i] at the end of nums1
+                nums1[k--] = nums1[i--];
             } else {
-                nums1[k--] = nums2[j--];  // Place nums2[j] at the end of nums1
+                nums1[k--] = nums2[j--];
             }
         }
 
-        // If nums2 still has remaining elements, copy them
         while (j >= 0) {
             nums1[k--] = nums2[j--];
         }
